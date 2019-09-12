2204292dc327d75ea781a242bfdf3b87500cd0f5
Eduardo Perez <edujpp1@gmail.com>
2019-09-12 17:53:23 -0300

    Se agregaron las responsabilidades mover() y perfervida() al objeto jugador que serán implementadas luego

--- js/jugador.js
+++ js/jugador.js
@@ -9,8 +9,7 @@ var Jugador = {
   ancho: 15,
   alto: 30,
   velocidad: 10,
-  vidas: 5,
-  // Hay que agregar lo que falte al jugador: movimientos, perdida de vidas,
-  // y todo lo que haga falta para que cumpla con sus responsabilidades
-
+  vidas: 5,  
+  mover: function () {},
+  perdervida: function () {}
 }
