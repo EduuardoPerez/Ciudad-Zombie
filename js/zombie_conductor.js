3a9d9e2adf6d3e694ae5e94528fcf7aea26f965c
Eduardo Perez <edujpp1@gmail.com>
2019-09-13 23:04:39 -0300

    Se implemento el movimiento de los zombies

--- js/zombie_conductor.js
+++ js/zombie_conductor.js
@@ -8,3 +8,23 @@ var ZombieConductor = function(sprite, x, y, ancho, alto, velocidad, rangoMov, d
   Enemigo.call(this, sprite, x, y, ancho, alto, velocidad, rangoMov);
   this.direccion = direccion;
 }
+
+// Se mueve de manera horizontal o vertical según la direccion indicada al ser instaciado el ZombieConductor
+ZombieConductor.prototype.mover = function() {
+  if (this.direccion==='h') {
+    this.x -= this.velocidad;
+  } if (this.direccion==='v') {
+    this.y += this.velocidad;
+  }
+
+  /* En esta parte lo que hacemos es invertir la direccion horizontal si
+  toca uno de sus limites, modificando su velocidad. Si multiplicamos por -1 la
+  velocidad lo que estamos haciendo es invertir su direccion.*/
+  if (this.direccion==='h' && (this.x < this.rangoMov.desdeX) || (this.x > this.rangoMov.hastaX)){
+    this.velocidad *= -1;
+  }
+  // Si sobrepasa el rangoY, lo manda al centro entre ambos rangos
+  if (this.direccion==='v' && (this.y < this.rangoMov.desdeY) || (this.y > this.rangoMov.hastaY)) {
+    this.velocidad *= -1;
+  }
+}
